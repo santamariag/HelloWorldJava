c772230681c90ceab4927f320e8b92bdb2c7a69e
shady1984 <75033970+shady1984@users.noreply.github.com>
2020-12-01 08:09:43 +0100

    Update HelloWorldPing.java

--- docker-git-hello-world/src/main/java/org/giuseppe/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/giuseppe/docker/HelloWorldPing.java
@@ -3,7 +3,7 @@ package org.giuseppe.docker;
 public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
-		for(int i = 0 ; i < 100; i++){
+		for(int i = 0 ; i < 200; i++){
 			System.out.println("Hello World Ping " + i );
 			Thread.sleep(1000);
 		}
